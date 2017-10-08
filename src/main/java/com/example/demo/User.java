5c0c884b557eda2bb8e9aa8fe94ccf4f1727cddf
Vladimir Litovkin <vladimir.litovkin@e-solutions.cz>
2017-10-08 21:34:24 +0200

    making User Entity

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,8 +1,30 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+    @Id
     private int id;
     private String name;
 
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
 }
