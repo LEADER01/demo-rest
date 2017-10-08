39d3ad95734aaec427dbd569c440ff885e728937
Vladimir Litovkin <vladimir.litovkin@e-solutions.cz>
2017-10-08 21:32:54 +0200

    adding UserController

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    //why not go for constructor based instead?
+
+
+    @RequestMapping(value = "/get-all-users", method = RequestMethod.GET)
+    public String response() {
+        return "all users";
+    }
+
+}
