5498642e618eb62d6458c1e1f4a0c8d96d8af834
Vladimir Litovkin <vladimir.litovkin@e-solutions.cz>
2017-10-08 21:40:52 +0200

    adding repository

--- src/main/java/com/example/demo/UserService.java
+++ src/main/java/com/example/demo/UserService.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
 
     private List<User> users;
 
